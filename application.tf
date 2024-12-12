318fa25891aa553d3a65f8663a4eda854a5fbb26
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-12-12 12:56:04 +0530

    Update application.tf

--- application.tf
+++ application.tf
@@ -1,6 +1,13 @@
 provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
+  host                   = "https://${google_container_cluster.primary.endpoint}"
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1" # Use the stable API version
+    command     = "gke-gcloud-auth-plugin"
+    args        = ["--v=1"]
+  }
+}
 
   exec {
     api_version = "client.authentication.k8s.io/v1" # Use the stable API version
